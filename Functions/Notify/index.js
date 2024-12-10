8a9ac370d4c2ec66b6e1086d783422b353f4bc1e
Mateusz <potasnik@student.agh.edu.pl>
2024-12-10 15:42:28 +0100

    mailjet -> sendgrid

--- Functions/Notify/index.js
+++ Functions/Notify/index.js
@@ -1,86 +1,59 @@
 const functions = require('@google-cloud/functions-framework');
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const sgMail = require('@sendgrid/mail');
 
-const { Firestore } = require('@google-cloud/firestore');
-
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 initializeApp({
-    credential: applicationDefault()
+  credential: applicationDefault()
 });
 
 const db = getFirestore();
 
-const Mailjet = require('node-mailjet');
-const mailjet = Mailjet.apiConnect(
-    "b7b730fcdf3a30d3333e36c3c77becf0",
-    "989626cb1e55bbff81f1d58530533191"
-);
 
+sgMail.setApiKey('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');
 
 functions.http('Notify', async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', '*');
 
-    if (req.method === 'OPTIONS') {
-        res.set('Access-Control-Allow-Methods', 'GET, POST');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        res.status(204).send('');
-    } else {
-        try {
-            const snapshot = await db.collection('tasks').get();
-            const currentDate = new Date();
-            const fourDaysFromNow = new Date();
-            fourDaysFromNow.setDate(currentDate.getDate() + 3);
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    return res.status(204).send('');
+  } else {
+    try {
+      const snapshot = await db.collection('tasks').get();
+      const currentDate = new Date();
+      const fourDaysFromNow = new Date();
+      fourDaysFromNow.setDate(currentDate.getDate() + 3);
 
-            const tasksWithDueDate = snapshot.docs
-                .filter(doc => {
-                    const dueDate = doc.data().due_to.toDate();
-                    return dueDate < fourDaysFromNow;
-                })
-                .map(doc => ({ id: doc.id, ...doc.data() }));
+      const tasksWithDueDate = snapshot.docs
+        .filter(doc => {
+          const dueDate = doc.data().due_to.toDate();
+          return dueDate < fourDaysFromNow;
+        })
+        .map(doc => ({ id: doc.id, ...doc.data() }));
 
+      let message = "";
+      tasksWithDueDate.forEach((item) => {
+        message += item.title + " - " + item.due_to.toDate().toDateString() + "<br />";
+      });
 
-            let message = "";
-            tasksWithDueDate.map((item, index) => {
-                message += item.title;
-                message += " ";
-                message += item.due_to.toDate().toDateString();
-                message += "<br />";
-            })
+      const msg = {
+        to: 'nnn.2321a@gmail.com',
+        from: 'potasnik@student.agh.edu.pl',
+        subject: 'A reminder of the expiring task deadlines!',
+        text: 'Tasks with expiring deadlines.',
+        html: `<h3>Tasks with expiring deadlines:</h3><br />${message}`
+      };
 
-            const request = mailjet
-                .post('send', { version: 'v3.1' })
-                .request({
-                    Messages: [
-                        {
-                            From: {
-                                Email: "potasnik@student.agh.edu.pl",
-                                Name: "GCP"
-                            },
-                            To: [
-                                {
-                                    Email: "nnn.2321a@gmail.com",
-                                    Name: "TODO APP USER"
-                                }
-                            ],
-                            Subject: "A reminder of the expiring task deadlines!",
-                            TextPart: "Yo",
-                            HTMLPart: "<h3>Tasks with expiring deadlines:</h3><br />" + message
-                        }
-                    ]
-                })
-            request
-                .then((result) => {
-                    console.log(result.body)
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
-            res.status(200).send('E-mail sent successfully');
-        } catch (error) {
-            console.error('Error when sending an email:', error);
-            res.status(500).send('Error when sending an email');
-        }
+      await sgMail.send(msg);
+      console.log("Email sent successfully");
+      res.status(200).send('E-mail sent successfully');
+    } catch (error) {
+      console.error('Error when sending an email:', error);
+      res.status(500).send('Error when sending an email');
     }
+  }
 });
